9768a53ea636dcc4371f42aa53c5380c74e95137
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:49:50 +0500

    Create PremiumAndDiscounts.h

---
+++ PremiumAndDiscounts.h
@@ -0,0 +1,8 @@
+#ifndef PREMIUMANDDISCOUNTS_H
+#define PREMIUMANDDISCOUNTS_H
+#include "Libraries.h"
+#include "GlobalObjectsAndVariables.h"
+#include "ConstantStrings.h"
+void premium(std::string login, std::string right);
+void discounts(std::size_t id, double product_price, short attempt);
+#endif // PREMIUMANDDISCOUNTS_H
