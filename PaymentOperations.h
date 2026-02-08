4e84f892d95c8156c33a46d7201c1b5b6c3d3845
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:49:18 +0500

    Create PaymentOperations.h

---
+++ PaymentOperations.h
@@ -0,0 +1,8 @@
+#ifndef PAYMENTOPERATIONS_H
+#define PAYMENTOPERATIONS_H
+#include "Libraries.h"
+#include "GlobalObjectsAndVariables.h"
+#include "ConstantStrings.h"
+void payment(std::string product, double product_price, std::size_t count);
+void sales();
+#endif // PAYMENTOPERATIONS_H
