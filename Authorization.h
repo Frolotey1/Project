84ce0e30ee6a242e5e3d0de768f2e4dded3babff
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:45:03 +0500

    Create Authorization.h

---
+++ Authorization.h
@@ -0,0 +1,9 @@
+#ifndef AUTHORIZATION_H
+#define AUTHORIZATION_H
+#include "ConstantStrings.h"
+#include "Libraries.h"
+#include "GlobalObjectsAndVariables.h"
+void login(short attempt);
+void registry(short attempt);
+void accounts(short attempt);
+#endif // AUTHORIZATION_H
