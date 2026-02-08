d1329a2f504fb6562321deb99d9819e804db4c70
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:48:55 +0500

    Create MomFriendSon.h

---
+++ MomFriendSon.h
@@ -0,0 +1,7 @@
+#ifndef MOMFRIENDSON_H
+#define MOMFRIENDSON_H
+#include "Libraries.h"
+#include "ConstantStrings.h"
+#include "GlobalObjectsAndVariables.h"
+void momFriendSon();
+#endif // MOMFRIENDSON_H
