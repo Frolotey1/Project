dd214d853662e33be593661570312f2e6299b557
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:48:27 +0500

    Create Libraries.h

---
+++ Libraries.h
@@ -0,0 +1,12 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <thread>
+#include <chrono>
+#include <sstream>
+#include <locale>
+#include <string>
+#endif // LIBRARIES_H
