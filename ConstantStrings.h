dd8c3430a26a3762bbcf64b135df62381dc6ce05
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:45:34 +0500

    Create ConstantStrings.h

---
+++ ConstantStrings.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTSTRINGS_H
+#define CONSTANTSTRINGS_H
+#include "Libraries.h"
+const std::string RED = "\033[31m";
+const std::string LIGHT_BLUE = "\033[36m";
+const std::string YELLOW = "\033[33m";
+const std::string GREEN = "\033[32m";
+const std::string RESET = "\033[0m";
+#endif // CONSTANTSTRINGS_H
