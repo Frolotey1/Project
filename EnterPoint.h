ef0ff3806750f4c0c71bd8afb28ef9a2a72f5643
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:46:34 +0500

    Create EnterPoint.h

---
+++ EnterPoint.h
@@ -0,0 +1,7 @@
+#ifndef ENTERPOINT_H
+#define ENTERPOINT_H
+#include "Libraries.h"
+#include "ConstantStrings.h"
+#include "GlobalObjectsAndVariables.h"
+void menu(short attempt);
+#endif // ENTERPOINT_H
