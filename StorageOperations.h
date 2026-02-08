798b1720ab7412d79945f44e6dbd401afa3a32e6
fr1zb3e <168219806+Frolotey1@users.noreply.github.com>
2026-02-08 19:50:52 +0500

    Create StorageOperations.h

---
+++ StorageOperations.h
@@ -0,0 +1,8 @@
+#ifndef STORAGEOPERATIONS_H
+#define STORAGEOPERATIONS_H
+#include "Libraries.h"
+#include "ConstantStrings.h"
+#include "GlobalObjectsAndVariables.h"
+void storage(short attempt);
+void start(short attempt);
+#endif // STORAGEOPERATIONS_H
